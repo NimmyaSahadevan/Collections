03b1ddf6806bf87b218a756db95f8254eeeb4548
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2020-01-08 16:18:12 +0530

    Program Using LinkedList

--- LinkedListExample_1.java
+++ LinkedListExample_1.java
@@ -1,20 +1,17 @@
 import java.util.*;
 public class LinkedListExample_1
 {
-
 	public static void main(String[] args) 
 	{
-		List word=new LinkedList(); //creating linked list
+		List<String> word=new LinkedList<String>(); //creating linked list
 		//adding elements into linked list
 		word.add("Competition");
 		word.add("Be Positive");
 		word.add("Happy Day");
-		Iterator itr=word.iterator();//creating iterator object
+		Iterator<String> itr=word.iterator();//creating iterator object
 		while(itr.hasNext())  //checks whether list contains next element
 		{
 			System.out.println(itr.next()); //prints next element
 		}
-
 	}
-
 }
