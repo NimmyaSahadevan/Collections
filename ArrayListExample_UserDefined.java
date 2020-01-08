b2528a8216c5a43cd077e3505a333d8158c3a118
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2020-01-08 12:16:32 +0530

    Program for Passing Class as type of ArrayList

---
+++ ArrayListExample_UserDefined.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Contact
+{
+	private String name;
+	private String phonenumber;
+	public Contact(String name,String phonenumber)  //constructor
+	{
+		this.name=name;
+		this.phonenumber=phonenumber;
+	}
+	//getters
+	public String getName()
+	{
+		return name;
+		
+	}
+	public String getphonenumber()
+	{
+		return phonenumber;
+	}
+}
+public class ArrayListExample_UserDefined 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Contact>contact=new ArrayList<Contact>();  //class as arraylist object
+		//adding values using objects of class
+		Contact firstcontact=new Contact("tim","123356");    
+		Contact secondcontact=new Contact("john","134677");
+		contact.add(firstcontact);
+		contact.add(secondcontact);
+		//getting the values from list
+		for(int i=0;i<contact.size();i++)
+		{
+			System.out.print(contact.get(i).getName()+" ");
+			System.out.print(contact.get(i).getphonenumber()+" ");
+		}
+	}
+}
